9874902e3b5fcff863f26d60da0efd2fb618ae9d
moises.alcocer <contacto@ironwoods.es>
2020-04-28 12:12:44 +0200

    added helper to compose the select options

---
+++ src/helpers/UI.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const getOptions = (data, labelName) =>
+{
+    const label = (labelName) ? labelName : 'label'
+
+    return (data)
+        ? data.map((optionObject, index) => {
+            const disable = (optionObject.disable) ? true : false;
+            const optionLabel = optionObject[label];
+            const optionValue = optionObject.key;
+
+            // eslint-disable-next-line react/react-in-jsx-scope
+            return <option
+                key={index}
+                value={optionValue}
+                disabled={disable}>
+                {optionLabel}
+            </option>
+        })
+        : '';
+}
