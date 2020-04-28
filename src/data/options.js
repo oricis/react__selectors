f89df96d1aec33962b9d40da0ef2b4ebe55bdf1d
moises.alcocer <contacto@ironwoods.es>
2020-04-28 12:11:51 +0200

    added data for the selects

---
+++ src/data/options.js
@@ -0,0 +1,23 @@
+export const langCodes = [
+    {
+        key: 'ES', label: 'Español'
+    },
+    {
+        key: 'EN', label: 'Inglés'
+    }
+];
+
+export const people = [
+    {
+        key: '', name: 'Select a person...', disable: true
+    },
+    {
+        key: 'foo', name: 'Foo', default: true
+    },
+    {
+        key: 'baz', name: 'Baz', default: true // will be selected the last "default"
+    },
+    {
+        key: 'page', name: 'Page', disable: true
+    }
+];
